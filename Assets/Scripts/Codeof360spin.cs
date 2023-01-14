ab646e7bda68a3e6a8d75fbb645b24f719fa3dcf
cyberplomo <yyeg214@gmail.com>
2023-01-14 05:37:44 +0300

    level 4 has been created

---
+++ Assets/Scripts/Codeof360spin.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Codeof360spin : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    private Quaternion rotation;
+
+    void Start()
+    {
+        //create rotation around x-axis
+        rotation = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.forward);
+        //Vector3.right x ekseni, Vector3.forward y ekseni, Vector3.up z ekseni
+    }
+    void Update()
+    {
+        //apply rotation to the cylinder
+        transform.rotation *= rotation;
+    }
+}
