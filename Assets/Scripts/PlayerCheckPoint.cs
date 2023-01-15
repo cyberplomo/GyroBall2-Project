f0eeb7aa0f036319565dd4d937a382e7f0ad128d
cyberplomo <yyeg214@gmail.com>
2023-01-15 20:45:26 +0300

    blabla

--- Assets/Scripts/PlayerCheckPoint.cs
+++ Assets/Scripts/PlayerCheckPoint.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+	
 
 public class PlayerCheckPoint : MonoBehaviour
 {
     public Transform chracter;
     
     private Vector3 spawnPoint;
+	
+	private Rigidbody rb;
+
+	public float speed = 0;	
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
+	
     void Update()
     {	//death area ile bu sorunu çözdüm.S
         //if (chracter.transform.position.y < -0.1f)
@@ -29,8 +35,11 @@ public class PlayerCheckPoint : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Enemy"))
         {
-            //Destroy(gameObject);
-            spawn();
+            //Destroy(gameObject);  
+			spawn();
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+            rb.Sleep();
 
         }
         
